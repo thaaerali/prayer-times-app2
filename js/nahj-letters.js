940f86380aa325a80d82156a5b4befa8f644deec
Thaaer Abdullah <zamenki@gmail.com>
2025-12-27 16:36:00 +0300

    Update nahj-letters.js

--- js/nahj-letters.js
+++ js/nahj-letters.js
@@ -1,6 +1,6 @@
 class NahjLetters {
     // استبدل رابط Netlify بالرابط الفعلي لموقعك
-    constructor(baseURL = 'https://shimmering-conkies-412365.netlify.app/nahj-data/') {
+    constructor(baseURL = 'https://thpryer.netlify.app/nahj-data/') {
         this.baseURL = baseURL;
         this.lettersIndexURL = this.baseURL + 'letters/letters-index.json';
         this.currentLetter = null;
@@ -113,4 +113,5 @@ if (typeof window !== 'undefined') {
     window.NahjLetters = NahjLetters;
     console.log('✅ NahjLetters جاهز للاستخدام مع Netlify');
 }
+
 
